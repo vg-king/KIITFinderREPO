0648cb6d625ce8f8484405b5eec5b99cb53d8997
vg-king <vg3772285@gmail.com>
2025-07-23 17:09:52 +0530

    ok

--- project/KittFinder/src/main/java/com/exampleKiitFinder/KittFinder/controller/UserController.java
+++ project/KittFinder/src/main/java/com/exampleKiitFinder/KittFinder/controller/UserController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:8081")
 public class UserController {
 
     @Autowired
